c444e9650c1dfbdba756f885908ebace93a4f477
SamuelPene <samuel.pene@outlook.com>
2024-01-06 20:36:25 +1300

    Increased dashbaord width, added styling variable to index.css, refactored dashboard styling, created teh base pricie component and order component UI, menu nav component font size change via media query.

--- src/Dashboard/Components/Order/Order.jsx
+++ src/Dashboard/Components/Order/Order.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import './order.css';
 
 function Order() {
-  return <div className="dashboard__order__container">Order</div>;
+  return (
+    <div className="dashboard__order__container">
+      <div className="order__coupon-wrapper">
+        <a>Promotions</a>
+      </div>
+      <div className="order__id__container">
+        <h3>My Order</h3>
+        <h3>Order ID:</h3>
+      </div>
+      <div className="order__item-wrapper">No Items Currently Selected</div>
+      <div className="order__delivery-wrapper">Pickup / Delivery</div>
+    </div>
+  );
 }
 
 export default Order;
--- src/Dashboard/Components/Price/Price.jsx
+++ src/Dashboard/Components/Price/Price.jsx
@@ -2,7 +2,27 @@ import React from 'react';
 import './price.css';
 
 function Price() {
-  return <div className="dashboard__price__container">Price</div>;
+  return (
+    <div className="dashboard__price__container">
+      <div className="price__text__container">
+        <div className="price__discount-wrapper">
+          <h5>No Discount Active</h5>
+        </div>
+        <div className="price__item-total-wrapper">
+          <h5>Item Total:</h5>
+        </div>
+        <div className="price__order-total-wrapper">
+          <h5>Order Total:</h5>
+        </div>
+        <div className="price__delivery-type-wrapper">
+          <h5>Select A Delivery Type</h5>
+        </div>
+      </div>
+      <div className="price__confirm-order-btn-wrapper">
+        <button>Confirm Order</button>
+      </div>
+    </div>
+  );
 }
 
 export default Price;
